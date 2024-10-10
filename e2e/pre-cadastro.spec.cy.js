b47078c549c34959d12f94d90ac6ff8ac77367a8
yuuwesley <yuuwesley@gmail.com>
2024-10-10 18:00:36 -0300

    inclui a funcionalidade pre cadastro

--- e2e/pre-cadastro.spec.cy.js
+++ e2e/pre-cadastro.spec.cy.js
@@ -14,8 +14,8 @@ describe('Funcionalidade Pré Cadastro', () => {
         cy.get(':nth-child(4) > .button').click();
 
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click();
-        cy.get('#account_first_name').type(faker.person.firstName());  // preciso descobrir o que usar nessa parte deprecated
-        cy.get('#account_last_name').type(faker.person.lastName());   // preciso descobrir o que usar nessa parte deprecated
+        cy.get('#account_first_name').type(faker.person.firstName());
+        cy.get('#account_last_name').type(faker.person.lastName());
         cy.get('#account_display_name').type(faker.internet.userName()); 
 
         cy.get('.woocommerce-Button').click();
